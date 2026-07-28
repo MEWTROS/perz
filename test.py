6dc92972b892f87d45ec6f57b1f02b7155e4f4c0
Sujal <sujalbandodkar05@gmail.com>
2026-07-28 11:36:23 +0530

    first push

---
+++ test.py
@@ -0,0 +1,4 @@
+n=20
+for i in range(n):
+    if i%2!=0 and i%3!=0:
+        print(i)
---
+++ test2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World"<<endl;
+    cout<<"C++ working";
+    return 0;
+    
+}
